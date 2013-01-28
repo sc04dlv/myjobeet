8c274ae1e8b956d84b3c14c4047472956607bf6f
lvdubov <sc04dlv@inbox.ru>
2013-01-28 19:17:28 +0400

    +pages +show page on front

---
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * main actions.
+ *
+ * @package    jobeet
+ * @subpackage main
+ * @author     Leonid dll@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class mainActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->page = Doctrine::getTable('Page')->findOneBySlug('contacts');
+    $this->forward404Unless($this->page);
+    
+  }
+}
---
+++ apps/frontend/modules/main/templates/indexSuccess.php
@@ -0,0 +1,6 @@
+<h1>Welcome!!!</h1>
+
+<h2><?php echo $page->getName() ?> </h2>
+<hr />
+<?php echo $page->getContent(ESC_RAW) ?>
+
